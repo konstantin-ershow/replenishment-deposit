d38bbfb5f86f317da3b49dc098c7e37efa215e40
Konstantin <kostik3600@mail.ru>
2021-08-01 11:58:22 +0300

    refactoring. use strict added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const formEl = document.getElementById('deposit-form');
 formEl.addEventListener('submit', handleSubmit);
 
@@ -17,47 +17,44 @@ function caluclateReplenismentDeposite(amount, period) {
     const fourthPeriod = 12;
     const fifthPeriod = 18;
     const firstPeriodPercent = 2;
-    const secondPeriodPercent=2.2;
-    const thirdPeriodPercent =2.3;
-    const fourthPeriodPercent =2.6;
-    const fifthPeriodPercent =2.7;
+    const secondPeriodPercent = 2.2;
+    const thirdPeriodPercent = 2.3;
+    const fourthPeriodPercent = 2.6;
+    const fifthPeriodPercent = 2.7;
     let percent = firstPeriodPercent;
-    if (period>=secondPeriod && period <thirdPeriod) {
+    if (period >= secondPeriod && period < thirdPeriod) {
         percent = secondPeriodPercent;
-    }
-    else if (period >=thirdPeriod && period<fourthPeriod) {
+    } else if (period >= thirdPeriod && period < fourthPeriod) {
         percent = thirdPeriodPercent;
-    }
-    else if (period>=fourthPeriod && period <fifthPeriod) {
+    } else if (period >= fourthPeriod && period < fifthPeriod) {
         percent = fourthPeriodPercent;
-    }
-    else if (period>=fifthPeriod) {
+    } else if (period >= fifthPeriod) {
         percent = fifthPeriodPercent;
     }
 
-    const monthsInYear =12;
-    const totalSum = amount*Math.pow(1+percent/(monthsInYear*100),period);
+    const monthsInYear = 12;
+    const totalSum = amount * Math.pow(1 + percent / (monthsInYear * 100), period);
     const profitSum = totalSum - amount;
     return {
-            totalSum: totalSum.toFixed(),
-            profitSum: profitSum.toFixed(),
-            percent: percent,
+        totalSum: totalSum.toFixed(),
+        profitSum: profitSum.toFixed(),
+        percent: percent,
     };
 }
 
-function handleSubmit (evt) {
+function handleSubmit(evt) {
     evt.preventDefault();
-    totalSumEl.textContent='';
-    profitEl.textContent='';
-    percentEl.textContent='';
+    totalSumEl.textContent = '';
+    profitEl.textContent = '';
+    percentEl.textContent = '';
 
     const amount = Number(amountInputEl.value);
     const period = Number(periodInputEl.value);
 
-    const result = caluclateReplenismentDeposite(amount,period);
+    const result = caluclateReplenismentDeposite(amount, period);
 
-    totalSumEl.textContent =`${result.totalSum}`;
-    profitEl.textContent =`${result.profitSum}`;
-    percentEl.textContent=`${result.percent}`;
+    totalSumEl.textContent = `${result.totalSum}`;
+    profitEl.textContent = `${result.profitSum}`;
+    percentEl.textContent = `${result.percent}`;
 
 }
